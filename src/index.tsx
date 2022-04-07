0084ac27c9046896a7201e950d94d0d346d9d3ef
Mohamed Hossam <m.hossameldine@gmail.com>
2022-04-07 15:12:26 +0200

    fix: upgrade rendering root element to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Can not find the root element!');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
