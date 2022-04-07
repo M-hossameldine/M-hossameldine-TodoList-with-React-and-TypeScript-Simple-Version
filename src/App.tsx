2a39e8a989b6a1e506508a3de6540327e6f6138c
Mohamed Hossam <m.hossameldine@gmail.com>
2022-04-07 12:01:29 +0200

    Feat(Todos): add todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/Todo';
+
+const todos = [new Todo('React'), new Todo('TypeScript')];
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import Todo from '../models/Todo';
+import TodoItem from './TodoItem';
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item: Todo) => (
+        <TodoItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
